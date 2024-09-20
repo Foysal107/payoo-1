784bcfed3bbc13d51acfa6b00baaac1e0d4f2248
Foysal107 <foysalmiah8443@gmail.com>
2024-09-20 21:25:59 +0600

    first commit

---
+++ home.js
@@ -0,0 +1,8 @@
+
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault(), value;
+
+
+    const addmoneyInput = document.getElementById('input-money').value;
+    console.log(addmoneyInput);
+});
